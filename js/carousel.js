83ea25b07e36c2a1f2de74e48cefa567112549a1
Stepan <empo999pr@gmail.com>
2020-05-01 18:24:04 +0300

    Carousell

---
+++ js/carousel.js
@@ -0,0 +1,34 @@
+const CAROUSEL_CLASS = 'saga-carousel';
+
+function init() {
+  const $carousels = Array
+    .prototype
+    .slice
+    .call(document.getElementsByClassName(CAROUSEL_CLASS));
+
+  if (!$carousels) {
+    return null;
+  }
+
+  const events = ['load', 'resize'];
+  const carousels = $carousels.map(($wrap) => {
+    return {
+      $wrap,
+      $list: $wrap.querySelector('*')
+    };
+  });
+
+  function justify() {
+    carousels.forEach(({$list}) => {
+      if ($list.scrollWidth > $list.clientWidth) {
+        $list.style.justifyContent = 'flex-start';
+      } else {
+        $list.style.justifyContent = 'center';
+      }
+    });
+  }
+
+  events.forEach((name) => window.addEventListener(name, justify));
+}
+
+init();
